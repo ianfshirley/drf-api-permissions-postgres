6ebf8abc2c74a68d3de53259cf6ccb54eb4a6ea1
ianfshirley <ianfshirley88@gmail.com>
2023-01-31 19:17:16 -0800

    drf setup complete

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import JobSerializer
+from .models import Job
 
-# Create your views here.
+
+class JobList(generics.ListCreateAPIView):
+    # A QuerySet represents a collection of objects from your database. It can have zero, one or many filters. Filters narrow down the query results based on the given parameters. In SQL terms, a QuerySet equates to a SELECT statement, and a filter is a limiting clause such as WHERE or LIMIT.
+    # The queryset that should be used for returning objects from this view.
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+class JobDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
